7d94d4a155d4922c497bdb07ccf14afbc113248c
aanilkumarreddy <aanilkumarreddy1@gmail.com>
2021-12-07 23:49:05 -0500

    removed static files

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ db.mongoose
     process.exit();
   });
 
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
